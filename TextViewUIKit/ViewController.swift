ac45251b38d721e32bd5125935d542c3949a7aac
Carrington <Manyuchi>
2023-07-06 14:04:09 +0200

    textView programmatically

--- TextViewUIKit/ViewController.swift
+++ TextViewUIKit/ViewController.swift
@@ -1,19 +1,57 @@
-//
 //  ViewController.swift
 //  TextViewUIKit
-//
 //  Created by DA MAC M1 157 on 2023/07/06.
-//
 
 import UIKit
 
 class ViewController: UIViewController {
+    
+    private let myTextView: UITextView = {
+        let textView = UITextView()
+        textView.translatesAutoresizingMaskIntoConstraints = false
+        textView.text = "TextView Programmatically"
+        textView.font = UIFont.boldSystemFont(ofSize: 17)
+        textView.textColor = .red
+        textView.textAlignment = NSTextAlignment.left
+        
+        textView.isEditable = true
+        textView.isSelectable = true
+        textView.allowsEditingTextAttributes = true
+        textView.clearsOnInsertion = false
+        
+        textView.layer.cornerRadius = 20
+//        textView.layer.shadowOffset = CGSize(width: 1, height: 2)
+//        textView.layer.shadowColor = UIColor.black.cgColor
+//        textView.layer.shadowOpacity = 0.4
+//        textView.layer.shadowRadius = 18
+        
+        textView.returnKeyType = UIReturnKeyType.done
+        
+        textView.dataDetectorTypes = [ .address, .calendarEvent, .flightNumber, .phoneNumber, .link]
+        textView.backgroundColor = .yellow
+        return textView
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        configureViews()
+        configureConstraints()
     }
-
+    
+    private func configureViews() {
+        view.addSubview(myTextView)
+    }
+    
+    private func configureConstraints() {
+        let myTextViewConstraints = [
+            myTextView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20),
+            myTextView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 20),
+            myTextView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -20),
+            myTextView.heightAnchor.constraint(equalToConstant: 120)
+        ]
+        
+        NSLayoutConstraint.activate(myTextViewConstraints)
+    }
 
 }
 
